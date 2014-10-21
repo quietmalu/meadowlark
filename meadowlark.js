7f581eebda375d2bdf41697ca267f43313ae3e32
quietmalu <dmathews@rogers.com>
2014-10-21 16:55:25 -0400

    Cleanup in meadowlark.js after fortunecookie extraction

--- meadowlark.js
+++ meadowlark.js
@@ -8,34 +8,6 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 //Static middleware:
 app.use(express.static(__dirname + '/public'));
-//app.get('/', function(req, res){
-//    res.type('text/plain');
-//    res.send('Meadowlark Travel');
-//});
-//app.get('/about', function(req, res){
-//    res.type('text/plain');
-//    res.send('About Meadowlark Travel');
-//});
-//// custom 404 page
-//app.use(function(req, res, next){
-//    res.type('text/plain');
-//    res.status(404);
-//    res.send('404 - Not Found');
-//});
-//// custom 500 page
-//app.use(function(err, req, res, next){
-//    console.error(err.stack);
-//    res.type('text/plain');
-//    res.status(500);
-//    res.send('500 - Server Error');
-//});
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
 app.get('/', function(req, res) {
     res.render('home');
 });
