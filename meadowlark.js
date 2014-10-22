957946c7f64b5a0af5eb354d21e04e6c217b5078
quietmalu <dmathews@rogers.com>
2014-10-22 10:38:02 -0400

    Added page testing plugins & test query route in middleware

--- meadowlark.js
+++ meadowlark.js
@@ -8,6 +8,12 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 //Static middleware:
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+// routes go here....
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
     res.render('home');
