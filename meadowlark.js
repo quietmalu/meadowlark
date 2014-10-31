abbf7f2918024fbc8f56e6c19ee1418b6a1300b7
quietmalu <dmathews@rogers.com>
2014-10-31 17:32:12 -0400

    Inserting handlebars sections in meadowlark

--- meadowlark.js
+++ meadowlark.js
@@ -85,3 +85,13 @@ function getWeatherData(){
         ]
     };
 }
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
