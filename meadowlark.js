1adb89668b9bfa7fd9e0ee5cbcd602f6560e2b50
quietmalu <dmathews@rogers.com>
2014-11-02 21:52:53 -0500

    Updated meadowlark from author after Ch07

--- meadowlark.js
+++ meadowlark.js
@@ -1,63 +1,9 @@
-var fortune = require('./lib/fortune.js');
 var express = require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
+
 // set up handlebars view engine
-var handlebars = require('express-handlebars')
-    .create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-app.set('port', process.env.PORT || 3000);
-//Static middleware:
-app.disable('x-powered-by');
-app.use(function(req, res, next){
-    res.locals.showTests = app.get('env') !== 'production' &&
-        req.query.test === '1';
-    next();
-});
-app.use(function(req, res, next){
-    if(!res.locals.partials) res.locals.partials = {};
-    res.locals.partials.weather = getWeatherData();
-    next();
-});
-//if( app.thing == null ) console.log( 'bleat!' );
-// routes go here....
-app.use(express.static(__dirname + '/public'));
-app.get('/headers', function(req,res){
-    res.set('Content-Type','text/plain');
-    var s = '';
-    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
-    res.send(s);
-});
-app.get('/', function(req, res) {
-    res.render('home');
-});
-app.get('/about', function(req, res) {
-    res.render('about', {
-        fortune: fortune.getFortune(),
-        pageTestScript: '/qa/tests-about.js'
-    });
-});
-app.get('/tours/hood-river', function(req, res){
-    res.render('tours/hood-river');
-});
-app.get('/tours/request-group-rate', function(req, res){
-    res.render('tours/request-group-rate');
-});
-// 404 catch-all handler (middleware)
-app.use(function(req, res, next){
-    res.status(404);
-    res.render('404');
-});
-// 500 error handler (middleware)
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
-app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.' );
-});
 var handlebars = require('express-handlebars').create({
     defaultLayout:'main',
     helpers: {
@@ -68,6 +14,21 @@ var handlebars = require('express-handlebars').create({
         }
     }
 });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+    next();
+});
+
+// mocked weather data
 function getWeatherData(){
     return {
         locations: [
@@ -94,4 +55,62 @@ function getWeatherData(){
             },
         ]
     };
-};
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+app.get('/about', function(req,res){
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    } );
+});
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+    res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+app.get('/jquery-test', function(req, res){
+    res.render('jquery-test');
+});
+app.get('/nursery-rhyme', function(req, res){
+    res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+    res.json({
+        animal: 'squirrel',
+        bodyPart: 'tail',
+        adjective: 'bushy',
+        noun: 'heck'
+    });
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
